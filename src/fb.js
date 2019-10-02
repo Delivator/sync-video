488a66b5df5b966087acbb4ec5be563335216983
Delivator <info@delivator.me>
2019-10-02 20:46:10 +0200

    BUG FIXES

--- src/fb.js
+++ src/fb.js
@@ -1,9 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-import firebaseConfig from "firebaseConfig";
+const firebaseConfig = require("./firebaseConfig");
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig.firebaseConfig);
 
 const db = firebase.firestore();
 const auth = firebase.auth();
@@ -12,12 +12,4 @@ const currentUser = auth.currentUser;
 const rooms = db.collection("rooms");
 const user_settings = db.collection("user_settings");
 
-export {
-  firebase,
-  db,
-  auth,
-  currentUser,
-  rooms,
-  user_settings,
-  firebaseConfig
-};
+export { firebase, db, auth, currentUser, rooms, user_settings };
